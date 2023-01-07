7670158db2c99e2b43f0c14c19641912a5bb9c3f
JL <joseluisreyes43@gmail.com>
2023-01-07 10:04:53 +0800

    added hello world

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fucker(someshit)
+{
+  int x = someshit + 1;
+  printf("%d\n", x);
+  return 0;
+}
+
+int main(void)
+{
+  printf("Hello world!\n");
+  fucker(4);
+  return 0;
+}
