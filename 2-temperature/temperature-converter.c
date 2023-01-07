a1c35f95db766dfa345d4e6fa01ee3c5dcf9f24e
JL <joseluisreyes43@gmail.com>
2023-01-07 10:49:21 +0800

    converted some variables into constants and changed from while to for loop

--- 2-temperature/temperature-converter.c
+++ 2-temperature/temperature-converter.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-// creates a temperature table for fahrenheit to celsius strating from OF
+// creates a temperature table for fahrenheit to celsius strating from O F
+
+// lower - lower limit of temp scale, upper - upper limit of temp scale, step - step sizes
+// constants
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+// int main()
+// {
+//   int fahrenheit, celsius;
+//   fahrenheit = LOWER;
+
+//   while (fahrenheit <= UPPER)
+//   {
+//     celsius = 5 * (fahrenheit - 32) / 9;
+//     printf("%d\t%d\n", fahrenheit, celsius);
+//     fahrenheit = fahrenheit + STEP;
+//   }
+
+//   return 0;
+// }
 
 int main()
 {
-  int fahrenheit, celsius;
-  int lower, upper, step;
+  int fahrenheit;
 
-  // lower - lower limit of temp scale, upper - upper limit of temp scale, step - step sizes
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahrenheit = lower;
-
-  while (fahrenheit <= upper)
+  for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP)
   {
-    celsius = 5 * (fahrenheit - 32) / 9;
-    printf("%d\t%d\n", fahrenheit, celsius);
-    fahrenheit = fahrenheit + step;
+    float celsius = (5.0 / 9.0 * (fahrenheit - 32));
+    printf("%3d %6.1f\n", fahrenheit, celsius);
   }
 
   return 0;
