23c51e42f395e3fc1634fc3806fa983b6863ec01
JL <joseluisreyes43@gmail.com>
2023-01-07 10:12:47 +0800

    commented out function calls, organized folders for executables

---
+++ 1-helloworld/helloworld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// function declaration in c
+
+// int fucker(someshit)
+// {
+//   int x = someshit + 1;
+//   printf("%d\n", x);
+//   return 0;
+// }
+
+int main(void)
+{
+  printf("Hello world!\n");
+  // function call in c
+  // fucker(4);
+  return 0;
+}
